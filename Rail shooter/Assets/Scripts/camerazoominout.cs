bf2d9f5f3455e3d3cb5878543d3df25785a00e07
Agent47 <66782335+Ashutosh-pixel@users.noreply.github.com>
2021-03-31 21:07:52 +0530

    boost key

---
+++ Rail shooter/Assets/Scripts/camerazoominout.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerazoominout : MonoBehaviour
+{
+    [SerializeField] private float boost;
+
+    [SerializeField] private bool boostconditon = true;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x,transform.localPosition.y,-20.33f);
+        float camboost = Input.GetAxis("Space");
+        float cameraboost = camboost * Time.deltaTime * boost;
+        transform.localPosition = new Vector3(transform.localPosition.x,transform.localPosition.y,cameraboost+transform.localPosition.z);
+    }
+}
--- Rail shooter/Assets/Scripts/zaxis.cs
+++ Rail shooter/Assets/Scripts/zaxis.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class zaxis : MonoBehaviour
 {
 
-    [SerializeField] private float speed;
+    [SerializeField] private float normalSpeed;
+    [SerializeField] private float boostSpeed;
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition = new Vector3(transform.localPosition.x,transform.localPosition.y,speed*Time.deltaTime + transform.localPosition.z);
+        transform.localPosition = new Vector3(transform.localPosition.x,transform.localPosition.y,normalSpeed*Time.deltaTime + transform.localPosition.z);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            normalSpeed = 800;
+            Invoke("Delay",6f);
+        }
+         float boost   = Input.GetAxis("Space");
+         float camspeed = boost*Time.deltaTime*boostSpeed;
+         transform.localPosition = new Vector3(transform.localPosition.x,transform.localPosition.y,camspeed+transform.localPosition.z);
+    }
+
+    void Delay()
+    {
+        normalSpeed = 500;
     }
 }
